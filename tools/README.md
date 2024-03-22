721a040fefd567faec8a7ab3e66ecbaebf155818
Ann Katrin Gagnat <ann.katrin.gagnat@nav.no>
2024-03-22 14:29:47 +0100

    Support list of form paths as script input. Print skjemanummer for missing forms in target db at end for easy copy-paste to byggers 'Opprett skjema' form

--- tools/README.md
+++ tools/README.md
@@ -12,7 +12,11 @@ Opprett en `.env`-fil som inneholder nødvendig informasjon for tilkobling til d
     TO_MONGO_CONNECTION_URL=mongodb+srv://<username>:<password>@<cluster-ref>.mongodb.net/<dbname>?retryWrites=true&w=majority
     TO_MONGO_DB_NAME=<dbname>
 
-Kall node-skriptet med skjema-path (f.eks. nav123456) som første argument.
+    # optional
+    FORM_PATHS=nav123456,nav123457b,nav123458
+
+Kall node-skriptet med skjema-path (f.eks. nav123456) som første argument, eller oppgi flere skjema-paths
+i miljøvariabel `FORM_PATHS` som vist i eksempelet ovenfor.
 
     > node index.js <skjema-path>
 
@@ -22,6 +26,54 @@ Kall node-skriptet med skjema-path (f.eks. nav123456) som første argument.
 * components
 * properties
 * modified
+
+# Output
+
+Eksempel på output fra skriptet:
+
+    Following forms will be copied: nav100706,nav230505,nav111218b,nav111222b
+    
+    Connected to source db
+    Connected to target db
+    
+    :: nav100706 ::
+    Lookup form nav100706 in source db...
+    Found form nav100706 in source db (modified Mon Feb 12 2024 15:16:30 GMT+0100 (Central European Standard Time))
+    Lookup form nav100706 in target db...
+    Found form nav100706 in target db (modified Mon Feb 12 2024 15:16:30 GMT+0100 (Central European Standard Time))
+    Form not modified in target db
+    
+    :: nav230505 ::
+    Lookup form nav230505 in source db...
+    Found form nav230505 in source db (modified Fri Sep 29 2023 11:10:46 GMT+0200 (Central European Summer Time))
+    Lookup form nav230505 in target db...
+    Found form nav230505 in target db (modified Fri Sep 29 2023 11:10:46 GMT+0200 (Central European Summer Time))
+    Form not modified in target db
+    
+    :: nav111218b ::
+    Lookup form nav111218b in source db...
+    Found form nav111218b in source db (modified Thu Mar 21 2024 10:17:59 GMT+0100 (Central European Standard Time))
+    Lookup form nav111218b in target db...
+    Form nav111218b not found in target db
+    
+    :: nav111222b ::
+    Lookup form nav111222b in source db...
+    Found form nav111222b in source db (modified Thu Mar 21 2024 10:17:34 GMT+0100 (Central European Standard Time))
+    Lookup form nav111222b in target db...
+    Found form nav111222b in target db (modified Fri Mar 22 2024 14:19:52 GMT+0100 (Central European Standard Time))
+    Form was modified in target db
+    
+    :: Summary ::
+    Number of updated forms: 1
+    Following forms had no updates: nav100706,nav230505
+    Following forms where missing in target DB:
+    * NAV 11-12.18B (nav111218b)
+    
+    The end
+
+Under 'Summary' ser man hvor mange skjemaer som ble kopiert, eventuelt hvilke skjema som ikke hadde noen endringer,
+samt en liste over skjemanummer som ikke eksisterer i databasen dit den skulle kopieres. Disse skjemanumrene kan man
+gå inn i byggeren og opprette tomme dummy-skjema med, og deretter kjøre skriptet på nytt.
 
 # Mulig utvidelser?
 
--- tools/index.js
+++ tools/index.js
@@ -3,10 +3,13 @@ const dotenv = require("dotenv");
 
 dotenv.config();
 
-const env = name => {
+const env = (name, defaultValue = "placeholder") => {
     const value = process.env[name];
     if (!value) {
-        console.log(`Missing env ${name}`);
+        if (defaultValue !== "placeholder") {
+            return defaultValue;
+        }
+        console.error(`Missing env ${name}`);
         process.exit(1);
     }
     return value;
@@ -22,53 +25,84 @@ const TARGET_DB = {
     dbName: env("TO_MONGO_DB_NAME"),
 }
 
+let FORM_PATHS = env("FORM_PATHS", "").split(",").map(p => p.trim()).filter(p => !!p);
+
 const clientSourceDb = new MongoClient(SOURCE_DB.url);
 const clientTargetDb = new MongoClient(TARGET_DB.url);
 
 async function main() {
     const args = process.argv.slice(2);
     const formPath = args[0];
-    if (!formPath) {
-        console.log("Missing argument: form path");
+    if (formPath) {
+        FORM_PATHS = [formPath];
+    } else if (!FORM_PATHS.length) {
+        console.error("Unable to resolve which forms to copy. Provide form path as argument, or set env FORM_PATHS as comma separated list of paths.");
         process.exit(1);
     }
-    const FORM_QUERY_FILTER = {path: formPath};
+    console.log("Following forms will be copied:", FORM_PATHS.join(","));
+    console.log("");
 
     await clientSourceDb.connect();
+
     console.log("Connected to source db");
     await clientTargetDb.connect();
     console.log("Connected to target db");
-
     const sourceDb = clientSourceDb.db(SOURCE_DB.dbName);
+
     const targetDb = clientTargetDb.db(TARGET_DB.dbName);
-    console.log(`Lookup form ${formPath} in source db...`);
     const sourceColForms = sourceDb.collection("forms");
-    const sourceForm = await sourceColForms.findOne(FORM_QUERY_FILTER);
-    if (sourceForm) {
-        console.log(`Found form ${formPath} in source db (modified ${sourceForm.modified})`);
-    } else {
-        console.log(`Form ${formPath} not found in source db`);
-        process.exit(1);
-    }
+    const missingInTargetDB = [];
+    const updatedForms = [];
+    const formsWithNoChange = [];
 
-    console.log(`Lookup form ${formPath} in target db...`);
-    const targetColForms = targetDb.collection("forms");
-    const targetForm = await targetColForms.findOne(FORM_QUERY_FILTER);
-    if (targetForm) {
-        console.log(`Found form ${formPath} in target db (modified ${targetForm.modified})`);
-        const formUpdateResult = await targetColForms.updateOne(FORM_QUERY_FILTER, {
-            $set: {
-                title: sourceForm.title,
-                components: sourceForm.components,
-                properties: sourceForm.properties,
-                modified: sourceForm.modified,
+    for (let i = 0; i < FORM_PATHS.length; i++) {
+        const path = FORM_PATHS[i];
+        const FORM_QUERY_FILTER = {path};
+        console.log("");
+        console.log(`:: ${path} ::`);
+        console.log(`Lookup form ${path} in source db...`);
+        const sourceForm = await sourceColForms.findOne(FORM_QUERY_FILTER);
+        if (!sourceForm) {
+            console.warn(`Form ${path} not found in source db`);
+            continue;
+        }
+        console.log(`Found form ${path} in source db (modified ${sourceForm.modified})`);
+
+        console.log(`Lookup form ${path} in target db...`);
+        const targetColForms = targetDb.collection("forms");
+        const targetForm = await targetColForms.findOne(FORM_QUERY_FILTER);
+        if (targetForm) {
+            console.log(`Found form ${path} in target db (modified ${targetForm.modified})`);
+            const formUpdateResult = await targetColForms.updateOne(FORM_QUERY_FILTER, {
+                $set: {
+                    title: sourceForm.title,
+                    components: sourceForm.components,
+                    properties: sourceForm.properties,
+                    modified: sourceForm.modified,
+                }
+            });
+            const { modifiedCount} = formUpdateResult;
+            console.log(`Form ${modifiedCount ? 'was' : 'not'} modified in target db`);
+            if (modifiedCount) {
+                updatedForms.push(path);
+            } else {
+                formsWithNoChange.push(path);
             }
-        });
-        console.log("formUpdateResult", formUpdateResult);
-    } else {
-        console.log(`Form ${formPath} not found in target db`);
+        } else {
+            console.warn(`Form ${path} not found in target db`);
+            missingInTargetDB.push(`${sourceForm.properties.skjemanummer} (${path})`);
+        }
     }
-
+    console.log("");
+    console.log(":: Summary ::");
+    console.log(`Number of updated forms: ${updatedForms.length}`);
+    if (formsWithNoChange.length) {
+        console.log(`Following forms had no updates: ${formsWithNoChange.join(",")}`);
+    }
+    if (missingInTargetDB.length) {
+        console.log(`Following forms where missing in target DB: \n * ${missingInTargetDB.join("\n * ")}`)
+    }
+    console.log("");
     return "The end";
 }
 
